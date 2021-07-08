b0ff6599592afc3c5ff39b5087f2a050af6e85a8
KonTroAl <kostya98295@mail.ru>
2021-07-08 18:09:18 +0300

    start work on task_4

---
+++ my_chat/chat/start_chat.py
@@ -0,0 +1,17 @@
+# Продолжаем работать над проектом «Мессенджер»:
+# Реализовать скрипт, запускающий два клиентских приложения: на чтение чата
+# и на запись в него.
+# Уместно использовать модуль subprocess);
+# Реализовать скрипт, запускающий указанное количество клиентских приложений.
+
+import subprocess
+
+
+def start_chat(n):
+    subprocess.Popen('C:\programming\PyQT\HW\my_chat\chat\my_server.py', shell=True, creationflags=subprocess.CREATE_NEW_CONSOLE)
+    # p2 = subprocess.Popen('C:\programming\PyQT\HW\my_chat\chat\my_client.py', shell=True)
+    while n > 0:
+        subprocess.Popen('C:\programming\PyQT\HW\my_chat\chat\my_client.py', shell=True, creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+n = 2
+start_chat(n)
