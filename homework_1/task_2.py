784d1cc2ac2ce0c34c2d1e40cd16f35e7dbdb729
KonTroAl <kostya98295@mail.ru>
2021-07-10 13:59:35 +0300

    fixed mistakes for task_2

--- homework_1/task_2.py
+++ homework_1/task_2.py
@@ -7,18 +7,11 @@ import ipaddress
 import subprocess
 
 
-def host_range_ping(host, number):
-    a = []
-    ipv4 = ipaddress.ip_address(host)
-    a.append(str(ipv4))
-    res = ipv4 + 1
-    a.append(str(res))
-    for i in range(number - 2):
-        res = res + 1
-        number = number - 1
-        a.append(str(res))
-    for i in a:
-        response = subprocess.Popen(['ping', str(i)], stdout=subprocess.PIPE)
+def host_range_ping(host, num):
+    subnet = ipaddress.ip_network(host)
+    subnet_list = list(subnet.hosts())
+    for i in range(num):
+        response = subprocess.Popen(['ping', str(subnet_list[i])], stdout=subprocess.PIPE)
         result = response.stdout.read().decode('cp866')
         if '(0% потерь)' in result:
             print('Узел доступен')
@@ -26,9 +19,6 @@ def host_range_ping(host, number):
             print('Узел недоступен')
 
 
-# val = input('Введите начальный ip-адрес: ')
-# num = input('Введите количество прослушиваемых ip-адресов: ')
-hosts = '80.0.1.1'
+hosts = '80.0.1.0/28'
 num = 3
-
 host_range_ping(hosts, num)
