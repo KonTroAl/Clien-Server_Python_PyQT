8d87be706e5d3c1be02d6a8ea9aa921fe2b264ae
KonTroAl <troshenkin.k@yandex.ru>
2021-08-10 20:38:02 +0500

    firts step into cx_Freeze

--- my_chat/chat_package/setup.py
+++ my_chat/chat_package/setup.py
@@ -1,7 +1,19 @@
-from setuptools import setup
+# from setuptools import setup
+import sys
+from cx_Freeze import setup, Executable
 
-setup(name = 'chat_package',
-      version = '0.1',
-      author = 'Konstantin Troshenkin',
-      packages=["src"]
-)
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+build_exe_options = {"packages": ["src"]}
+
+setup(name='chat_package',
+      version='0.0.1',
+      author='Konstantin Troshenkin',
+      description='Chat application',
+      options={
+          'build_exe': build_exe_options
+      },
+      executables=[Executable('client.py', base=base)]
+      )
