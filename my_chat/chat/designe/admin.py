0e9249dcfd7628e290b14e7ed2458040ffc31354
KonTroAl <troshenkin.k@yandex.ru>
2021-08-11 20:38:52 +0500

    done with connecting to db and server

--- my_chat/chat/designe/admin.py
+++ my_chat/chat/designe/admin.py
@@ -1,11 +1,12 @@
 import sys
+import multiprocessing
+from threading import Thread
 
 from PyQt5 import QtGui, QtWidgets, QtCore
 from PyQt5.QtCore import Qt, QObject, QThread, pyqtSignal, pyqtSlot
 
 import importlib.util
 import importlib
-
 
 from admin_panel import Ui_MainWindow
 
@@ -20,7 +21,6 @@ def module_from_file(module_name, file_path):
 my_server = module_from_file('my_server', '../my_server.py')
 
 
-
 class AdminDialog(QtWidgets.QDialog):
 
     def __init__(self, parent=None):
@@ -28,40 +28,32 @@ class AdminDialog(QtWidgets.QDialog):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.PortConnectionButton.clicked.connect(self.start_connection)
+        self.ui.PortConnectionButton.setEnabled(False)
         self.ui.DBConnectionButton.clicked.connect(self.start_connection)
-        # self.ui.PortConnectionButton.clicked.connect(self.stop_connection)
-        # self.ui.DBConnectionButton.clicked.connect(self.start_db_connection)
-        # self.ui.DBConnectionButton.clicked.connect(self.stop_db_connection)
+
         self.start = None
         self.start_db = False
-        self.is_active = False
-
 
     def start_server(self):
         port = self.ui.PortTextBox.text()
         start = my_server.main(port)
+
         return start
 
     def start_DB(self):
-        pass
+        dialect = self.ui.DialectDBTextBox.text()
+        name_db = self.ui.NameDBTextBox.text()
+        session = my_server.main_db(dialect, name_db)
+        return session
 
     def start_connection(self):
 
-        if not self.start_db:
-            pass
-
-        self.ui.PortConnectionButton.setEnabled(True)
+        self.start = Thread(target=self.start_server)
+        self.start.daemon = True
+        self.start.start()
 
         self.start_db = self.start_DB()
-
-        self.start = self.start_server()
-        self.thread = QThread()
-        self.start.moveToThread(self.thread)
-
-        self.thread.started.connect(self.start)
-
-        self.thread.start()
-
+        self.ui.PortConnectionButton.setEnabled(True)
 
 
 if __name__ == '__main__':
